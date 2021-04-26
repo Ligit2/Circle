ff3b731de692b10040155d9829db6c177d3c19a4
lusine-muradyan <muradyanlusine@mail.ru>
2021-04-26 18:00:09 +0400

    Circle

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int pi = 3;
+	int radius = 3;
+	int area, perimete;
+	area = pi * radius * radius;
+	perimete = 2 * pi *radius;
+	System.out.println(area);
+	System.out.println(perimete);
+    }
+}
